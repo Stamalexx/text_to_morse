e1aab0296c7e0305d98f62c36c6ddcda19ec75a7
Stamalexx <stamelosalexios@gmail.com>
2025-05-08 09:52:32 +0300

    start

---
+++ main.py
@@ -0,0 +1,37 @@
+morse_dict = {
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..'
+}
+morse_output = ""
+user_input = input("Enter the word that you want to convert to morse code: ").upper()
+for letter in user_input:
+    if letter in morse_dict:
+        morse_output += morse_dict[letter]
+    else:
+        print(f"Letter {letter} not in the morse dic")
+
+print(morse_output)
